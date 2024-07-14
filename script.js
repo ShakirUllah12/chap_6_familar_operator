4be887c5ce6329a5c2788d7e35a9598eec49fff6
Shakir Ullah <shakirullahaup@gmail.com>
2024-07-15 01:10:44 +0500

    chap_6_done

---
+++ script.js
@@ -0,0 +1,62 @@
+// chapter  no 5 familar operator
+
+// (Addition operator)
+// Example
+
+const firstNumber=3;
+const secondNumber=2;
+const result=firstNumber+secondNumber;
+console.log(result);
+
+
+// if any one value is a string
+
+/
+
+const result= 2+ "17";
+console.log(result);
+ 
+const result=("71") + 2+2;
+console.log(result);
+
+const firstNumber="71";
+const secondNumber=2+2;
+const result=firstNumber+secondNumber;
+console.log(result);
+
+const  result= 3+2 +"60";
+console.log(result);
+
+const result=2+4 +"7" *9;
+// console.log(result);
+alert("result");
+
+// if any operator is a valid number 
+
+const firstNumber=88;
+const secondNumber=3;
+const result=firstNumber - secondNumber;
+ console.log(result);
+
+const result ="15" -6;
+console.log(result);
+
+
+// multiplication
+
+const firstNumber=2;
+const secondNumber=5;
+const result=firstNumber * secondNumber;
+console.log(result);
+
+const result="8"*"10";
+console.log(result);
+
+
+
+// division
+
+const dividen=15;
+const divisor=12;
+const result = 15 / 12 ;
+console.log(result);
